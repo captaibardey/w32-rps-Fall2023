a81dbfdd69eab32d69566fa8b9a10ce403fded1b
Ronan Bennett <ronan.r.bennett@gmail.com>
2023-11-15 11:11:37 -0800

    implement getThrowsComputer()

--- rpsSupport/getThrows.py
+++ rpsSupport/getThrows.py
@@ -1,7 +1,12 @@
-def getThrowsRPS() -> str:
+import random 
+
+def getThrowPlayer() -> str:
     print("Please type your throw: 'rock', 'paper', or 'scissors'")
     input_str = input().lower() 
     while input_str not in set(['rock', 'paper', 'scissors']): # TODO get this from the dict keys once that's decided
         print("Invalid input: please try again with input 'rock', 'paper', 'scissors'")
         input_str = input().lower() 
     return input_str
+
+def getThrowComputer() -> str:
+    return random.choice(['rock', 'paper', 'scissors'])
