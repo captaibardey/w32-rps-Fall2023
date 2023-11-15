c4a82da3120862c99a9d949cdb6a83e4ae9d7d53
Ronan Bennett <ronan.r.bennett@gmail.com>
2023-11-15 11:21:44 -0800

    clean up getThrows

--- rpsSupport/getThrows.py
+++ rpsSupport/getThrows.py
@@ -2,14 +2,12 @@ import rules
 import random 
 
 def getThrowPlayer() -> str:
-    print(f"Please type your throw: {', '.join(rules.rules_dict.keys())}")
+    print(f"Please type your throw: one of {', '.join(rules.rules_dict.keys())}")
     input_str = input().lower() 
     while input_str not in rules.rules_dict.keys():
-        print(f"Invalid input: please try again with input {', '.join(rules.rules_dict.keys())}")
+        print(f"Invalid input: please try again with one of {', '.join(rules.rules_dict.keys())}")
         input_str = input().lower() 
     return input_str
 
 def getThrowComputer() -> str:
     return random.choice(rules.rules_dict.keys())
-
-getThrowPlayer()
