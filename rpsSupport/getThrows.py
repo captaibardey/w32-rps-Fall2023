ad58449bb5d020f7cbe2d0a2cf2c3186a6740882
Ronan Bennett <ronan.r.bennett@gmail.com>
2023-11-15 11:00:52 -0800

    implement getThrows.py

---
+++ rpsSupport/getThrows.py
@@ -0,0 +1,7 @@
+def getThrowsRPS() -> str:
+    print("Please type your throw: 'rock', 'paper', or 'scissors'")
+    input_str = input().lower() 
+    while input_str not in set(['rock', 'paper', 'scissors']): # TODO get this from the dict keys once that's decided
+        print("Invalid input: please try again with input 'rock', 'paper', 'scissors'")
+        input_str = input().lower() 
+    return input_str
