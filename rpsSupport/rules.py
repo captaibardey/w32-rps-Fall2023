a124c117e93dde04f7f58c9c3b8d78176b6c3757
Anh Tran <anhptran01@gmail.com>
2023-11-16 10:45:16 -0800

    added rpsls

--- rpsSupport/rules.py
+++ rpsSupport/rules.py
@@ -1,10 +1,17 @@
 # -*- coding: utf-8 -*-
 
-#dictionary for rock paper scissors game
-rules_dict = {
+#dictionary for rock paper scissors lizard spock game
+rules_dict_RPSLS = {
     'rock': ['scissors', 'lizard'],
     'paper': ['rock', 'spock'],
     'scissors': ['paper', 'lizard'],
     'lizard': ['spock', 'paper'],
     'spock': ['scissors' , 'rock']
+    }
+
+#dictionary for rock paper scissors game
+rules_dict_RPS = {
+    'rock': ['scissors'],
+    'paper': ['rock'],
+    'scissors': ['paper']
     }
